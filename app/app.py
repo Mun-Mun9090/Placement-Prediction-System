fb86254ac4158d64e3b05abb71022ce39ccb29fe
Mun-Mun9090 <munmunnayak9090@gmail.com>
2026-08-05 12:19:22 +0530

    eda part completed

--- app/app.py
+++ app/app.py
@@ -4,14 +4,22 @@ import sys
 # Add project root directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from src.data.load_data import load_data, get_summary
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
+# Also serve charts folder as static
+charts_path = os.path.join(os.path.dirname(__file__), 'charts')
+app.config['CHARTS_FOLDER'] = charts_path
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route('/charts/<filename>')
+def serve_chart(filename):
+    charts_path = os.path.join(os.path.dirname(__file__), 'charts')
+    return send_from_directory(charts_path, filename)
 
 @app.route("/dataset")
 def dataset():
